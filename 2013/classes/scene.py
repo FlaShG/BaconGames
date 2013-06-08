5f6eece75b38d6b09dacf74a3009d36591bd38f3
Sascha Graeff <sascha@blackicegames.de>
2013-06-08 15:42:54 +0200

    Fixed camera zoom, reverted scene entity collection to an array.

--- 2013/classes/scene.py
+++ 2013/classes/scene.py
@@ -3,7 +3,7 @@ from classes.entity import Entity
 
 class Scene(object):
     def __init__(self):
-        self.entities = set()
+        self.entities = []
         self.camera = None
         self.set_camera(Camera())
 
@@ -15,7 +15,7 @@ class Scene(object):
         
     def add(self, entity):
         if(not entity in self.entities):
-            self.entities.add(entity)
+            self.entities.append(entity)
         
         
             
@@ -28,7 +28,7 @@ class Scene(object):
         for e in self.entities:
             e.onupdate(dt)
         for e in self.entities:
-            scale = window.height / 20.0 * self.camera.zoom
+            scale = window.height / (20.0 + self.camera.zoom)
             e.ondraw(window, sf.Transform().translate(window.size/2.0 - self.camera.position).scale(sf.Vector2(scale, scale)))
 
             
