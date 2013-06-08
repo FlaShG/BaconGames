61df18742d9dc708e5224fad0441f0608101f629
Anne-Victoria Meyer <2meyer@informatik.uni-hamburg.de>
2013-06-08 18:18:38 +0200

    Lightcircle added

--- 2013/classes/player.py
+++ 2013/classes/player.py
@@ -1,5 +1,5 @@
 from classes.input import Input
-from classes.entity import SpriteEntity
+from classes.entity import Entity, SpriteEntity
 from classes.texturemanager import TextureManager as TM
 import sfml as sf
 
@@ -13,3 +13,10 @@ class Player(SpriteEntity):
         hor = Input.get_axis('horizontal')
         ver = Input.get_axis('vertical')
         self.move(sf.Vector2(hor, ver)*dt*self.speed)
+
+class LightCircle(Entity):
+    def __init__(self):
+        super(LightCircle, self).__init__(layer = 50)
+        self.circle = SpriteEntity(texture = TM.get'circle.png')
+
+    
