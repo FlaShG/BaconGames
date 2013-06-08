d54443d32c3d25bf257d691b9a12a96e0b79bb25
Lars Thoms <lars.thoms@spacecafe.org>
2013-06-08 02:29:16 +0200

    add fancy text

--- 2013/__init__.py
+++ 2013/__init__.py
@@ -5,20 +5,27 @@ import sfml as sf
 window = sf.RenderWindow(sf.VideoMode(640, 480), "pySFML Window")
 
 try:
-   # load a sprite to display
-   texture = sf.Texture.from_file("rtm.png")
-   sprite = sf.Sprite(texture)
+    # load a sprite to display
+    texture = sf.Texture.from_file("rtm.png")
+    sprite = sf.Sprite(texture)
+
+    # text
+    font = sf.Font.from_file("aller.ttf")
+    text = sf.Text("Hello Sascha", font, 50)
+    text.position = sf.Vector2(250, 300)
+
 
 except IOError: exit(1)
 
 # start the game loop
 while window.is_open:
-   # process events
-   for event in window.events:
-      # close window: exit
-      if type(event) is sf.CloseEvent:
-         window.close()
+    # process events
+    for event in window.events:
+        # close window: exit
+        if type(event) is sf.CloseEvent:
+            window.close()
 
-   window.clear() # clear screen
-   window.draw(sprite) # draw the sprite
-   window.display() # update the window
+    window.clear() # clear screen
+    window.draw(sprite) # draw the sprite
+    window.draw(text)
+    window.display() # update the window
