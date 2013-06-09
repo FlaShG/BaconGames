c7ec9cc84022400ac4c10fc199fbd2b1ba060047
Lars Thoms <lars.thoms@spacecafe.org>
2013-06-09 16:13:01 +0200

    modified map

--- 2013/classes/animation.py
+++ 2013/classes/animation.py
@@ -7,30 +7,34 @@ from classes.texturemanager import TextureManager as TM
 
 class AnimatorEntity(SpriteEntity):
 
-    clip = []
+    clips = []
     timer = 0.0
     clip_id = 0
+    play_id = 0
 
     def __init__(self, path, quantity, interval):
         super(AnimatorEntity, self).__init__(texture=TM.get(path + '_0.png'))
-        self.quantity = quantity
-        self.path = path
-        self.interval = interval
-        self.gen_clip()
+        self.gen_clip(path=path, quantity=quantity, interval=interval)
 
 
-    def gen_clip(self):
-        for i in range(self.quantity):
-            self.clip.append(TM.get(self.path + '_%d' % i + '.png'))
+    def play(self, play_id):
+        self.play_id = play_id
+
+
+    def gen_clip(self, path, quantity, interval):
+        tmp = []
+        for i in range(quantity):
+            tmp.append(TM.get(path + '_%d' % i + '.png'))
+        self.clips.append(dict(textures=tmp, quantity=quantity, interval=interval))
 
 
     def update(self, dt):
         self.timer += dt
 
-        if(self.timer > self.interval):
+        if(self.timer > self.clips[self.play_id]['interval']):
             self.timer = 0.0
-            if(self.clip_id == (self.quantity - 1)):
+            if(self.clip_id == (self.clips[self.play_id]['quantity'] - 1)):
                 self.clip_id = 0
             else:
                 self.clip_id += 1
-            self.sprite.texture = self.clip[self.clip_id]
+            self.sprite.texture = self.clips[self.play_id]['textures'][self.clip_id]
